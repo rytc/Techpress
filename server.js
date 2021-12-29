bc4bf9e676f9c2a7d971cbead04aba15463133d7
rytc <rytc@protonmail.com>
2021-12-29 11:15:15 -0800

    Initial  commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv")
+const express = require("express")
+const path = require("path")
+
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(require("./controllers"))
+
+app.listen(process.env.PORT || 3000)
